afe03c75755be199c621bb6cfeb4d2766e684f79
Noobmine1@yandex.ru <Noobmine1@yandex.ru>
2024-09-28 18:55:46 +0300

    Добавьте файлы проекта.

---
+++ Sea batle/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Sea_batle
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
